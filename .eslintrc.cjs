de0c1e9192f84aae03e5b2e4bc973ec6e0f99f0e
jwbaeck <jwbaeck97@gmail.com>
2024-07-25 22:46:24 +0900

    Setting: Boilerplate

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  ignorePatterns: [".eslintrc.cjs", "src/index.css", "tailwind.config.js"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json", "./tsconfig.app.json", "./tsconfig.node.json"],
+  },
+  plugins: ["react-refresh", "@typescript-eslint"],
+  rules: {
+    "no-console": ["error", { allow: ["warn", "error", "info"] }],
+    "react/prop-types": "off",
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+  globals: {
+    chrome: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>This is Web</h1>;
+}
+
+export default App;
