3621a38e6d652161487573a98a2fc14cf0dd2fe4
jwbaeck <jwbaeck97@gmail.com>
2024-07-31 00:50:59 +0900

    Fix: Restrict Home Button on Initial Page

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,14 +1,17 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { IoMdHome } from "react-icons/io";
 
 import FigDiffLogo from "/logo.png";
 
 const Header: React.FC = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleHomeClick = () => {
     navigate("/dash");
   };
+
+  const isInitialPage = location.pathname === "/";
 
   return (
     <header className="flex items-center justify-between h-16 font-bold border-b border-gray-300 pl-4 pr-4">
@@ -16,7 +19,12 @@ const Header: React.FC = () => {
         <img src={FigDiffLogo} alt="FigDiff Logo" className="h-10 mr-3" />
         <h1>FigDiff</h1>
       </div>
-      <IoMdHome className="text-3xl cursor-pointer" onClick={handleHomeClick} />
+      {!isInitialPage && (
+        <IoMdHome
+          className="text-3xl cursor-pointer"
+          onClick={handleHomeClick}
+        />
+      )}
     </header>
   );
 };
