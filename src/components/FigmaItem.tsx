e475095cd210ca3f4dcaefa8b830d627307ba565
seohag <sscgeum@gmail.com>
2024-07-31 03:56:30 +0900

    Fix: Add History Delete Function

--- src/components/FigmaItem.tsx
+++ src/components/FigmaItem.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
-import DeleteButton from "./DeleteButton";
+import DeleteModal from "../components/DeleteModal";
+import { IoIosClose } from "react-icons/io";
 
 interface FigmaItemProps {
   userId: string;
@@ -17,6 +17,7 @@ const FigmaItem: React.FC<FigmaItemProps> = ({
   onDelete,
 }) => {
   const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
 
   const handleItemClick = () => {
     navigate(`/dash/${userId}/history/${pageName}`);
@@ -24,7 +25,16 @@ const FigmaItem: React.FC<FigmaItemProps> = ({
 
   const handleDeleteClick = (event: React.MouseEvent) => {
     event.stopPropagation();
+    setShowModal(true);
+  };
+
+  const handleToggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  const handleConfirmDelete = () => {
     onDelete(pageName);
+    setShowModal(false);
   };
 
   return (
@@ -32,18 +42,26 @@ const FigmaItem: React.FC<FigmaItemProps> = ({
       className="border rounded-lg p-4 shadow-sm hover:shadow-md transition-shadow duration-200 relative"
       onClick={handleItemClick}
     >
-      <DeleteButton
-        pageName={pageName}
-        className="absolute top-0 right-0 m-0.5 p-0.5"
-      />
       <h2 className="font-bold text-xl mb-2">{pageName}</h2>
       <p>저장된 리소스: {tabCount}개</p>
       <button
         onClick={handleDeleteClick}
-        className="absolute top-2 right-2 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
+        className="absolute top-2 right-2 bg-red-500 hover:bg-red-700 text-white text-2xl font-bold py-1 px-2 rounded"
       >
-        X
+        <IoIosClose />
       </button>
+      {showModal && (
+        <DeleteModal
+          userId={userId}
+          pageName={pageName}
+          tabUrlName=""
+          historyName=""
+          isOpen={showModal}
+          onCancel={handleToggleModal}
+          onDelete={handleConfirmDelete}
+          onToggle={handleToggleModal}
+        />
+      )}
     </div>
   );
 };
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -3,8 +3,17 @@ import axios from "axios";
 import FigmaItem from "../components/FigmaItem";
 import { useParams } from "react-router-dom";
 
+interface PageData {
+  pageName: string;
+  tabUrls: string[];
+}
+
+interface UserData {
+  pageNames: PageData[];
+}
+
 const DashBoardPage: React.FC = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<UserData | null>(null);
   const { userId } = useParams();
 
   useEffect(() => {
@@ -51,11 +60,6 @@ const DashBoardPage: React.FC = () => {
 
   return (
     <div className="p-4 h-full">
-      <div className="flex justify-end">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          팀 초대하기
-        </button>
-      </div>
       <div className="grid grid-cols-4 gap-4">
         {data.pageNames.map((page) => (
           <FigmaItem
