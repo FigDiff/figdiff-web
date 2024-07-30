674ad5ecf3442d6e01414f95a7d9cdea2cf94bfb
jwbaeck <jwbaeck97@gmail.com>
2024-07-30 17:00:51 +0900

    Chore: Adjust Logo location on Header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -2,7 +2,7 @@ import FigDiffLogo from "/logo.png";
 
 const Header: React.FC = () => {
   return (
-    <header className="flex items-center justify-center h-16 font-bold border-b border-gray-300">
+    <header className="flex items-center justify-start h-16 font-bold border-b border-gray-300 pl-4">
       <img src={FigDiffLogo} alt="FigDiff Logo" className="h-10 mr-3" />
       <h1>FigDiff</h1>
     </header>
